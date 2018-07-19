f47eeaf54078925ceec4274fce66f06f1fbcbb14
Jérémie Drouet <jeremie.drouet@gmail.com>
2018-07-19 17:38:43 +0200

    feat(start): start worker and web server by default

--- source/index.js
+++ source/index.js
@@ -1,30 +1,39 @@
 /* eslint-disable no-console */
 
-const startWeb = () => new Promise((resolve, reject) => {
-  const web = require('./web');
-  const config = require('./config');
+const startWeb = (cmd) => {
+  if (!['web', 'all'].includes(cmd)) {
+    return Promise.resolve();
+  }
+  return new Promise((resolve, reject) => {
+    const web = require('./web');
+    const config = require('./config');
 
-  web.listen(config.get('port'), (err) => {
-    if (err) return reject(err);
-    return resolve(`web listening on port ${config.get('port')}`);
+    web.listen(config.get('port'), (err) => {
+      if (err) return reject(err);
+      console.log(`web listening on port ${config.get('port')}`);
+      return resolve();
+    });
   });
-});
+};
 
-const startWorker = () => {
+const startWorker = (cmd) => {
+  if (!['worker', 'all'].includes(cmd)) {
+    return Promise.resolve();
+  }
   const worker = require('./worker');
-  return worker.listen().then(() => 'worker listening');
+  return worker.listen().then(() => console.log('worker listening'));
 };
 
 const start = (cmd) => {
-  if (cmd === 'web') return startWeb();
-  if (cmd === 'worker') return startWorker();
-  return Promise.reject(new Error('invalid command'));
+  return Promise.all([
+    startWeb(cmd),
+    startWorker(cmd),
+  ]);
 };
 
-const command = process.argv.length > 2 ? process.argv[2] : 'web';
+const command = process.argv.length > 2 ? process.argv[2] : 'all';
 
 start(command)
-  .then(console.log)
   .catch((err) => {
     console.error(err);
     process.exit(1);
